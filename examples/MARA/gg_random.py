778ce8eb6610bdd8309b880e3e5536aad9dd0763
Calvin <tacalvin123@gmail.com>
2019-04-04 10:36:30 -0700

    Test Random Target

--- examples/MARA/gg_random.py
+++ examples/MARA/gg_random.py
@@ -5,11 +5,11 @@ for an explanation of this script"""
 import gym
 import gym_gazebo2
 import time
-env = gym.make('MARA-v0')
+#env = gym.make('MARA-v0')
 #env = gym.make('MARAOrient-v0')
 #env = gym.make('MARACollision-v0')
 #env = gym.make('MARACollisionOrient-v0')
-#env = gym.make('MARACollisionOrientRandomTarget-v0')
+env = gym.make('MARACollisionOrientRandomTarget-v0')
 
 while True:
     # take a random action
